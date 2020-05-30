3556132000e6115bc43e3b3832df65fba9f1fff9
Gui Recordon <guilherme.recordon@gmail.com>
2020-05-30 17:25:55 -0300

    Create main file from where Glamourly class is called to run the linter

--- lib/linter.rb
+++ lib/linter.rb
@@ -1,71 +1,124 @@
 require 'strscan'
 
-text_file = File.open('../user.txt')
-text = text_file.read
-one_liner = text.gsub(/\n/, ' lyny ')
-text_clean = one_liner.gsub(/[^0-9a-z\n ]/i, '')
+class Glamourly
+  
+  def initialize(string)
+    @s = StringScanner.new(string)
+    @liner = 1
+    @error_level1 = 0
+    @error_level2 = 0
+    @array = []
+    @level1_hash =
+    {
+      'happy' => ['playful', 'content'],
+      'sad' => ['lonely', 'vulnerable'],
+      'disgusted' => ['disapproving', 'disappointed'],
+      'angry' => ['let down', 'resentful'],
+      'fearful' => ['scared', 'anxious'],
+      'bad' => ['bored', 'busy'],
+      'surprised' => ['startled', 'confused']
+    }
 
-s = StringScanner.new(text_clean)
-
-liner = 1
-error1 = 0
-error2 = 0
-array = []
-
-# p text_clean
-
-until s.eos? do
-  s.scan(/\s+/)
-  array << s.scan(/\w+/)
-end
-
-level1_hash =
-  {
-    'happy' => ['playful', 'content'],
-    'sad' => ['lonely', 'vulnerable'],
-    'disgusted' => ['disapproving', 'disappointed'],
-    'angry' => ['let down', 'resentful'],
-    'fearful' => ['scared', 'anxious'],
-    'bad' => ['bored', 'busy'],
-    'surprised' => ['startled', 'confused']
-  }
-
-level2_hash =
-  {
-    'playful' => ['aroused', 'cheeky'],
-    'content' => ['free', 'joyful'],
-    'lonely' => ['isolated', 'abandoned'],
-    'vulnerable' => ['victimized', 'fragile']
-  }
-
-array.each do |word|
-  if word == 'lyny'
-    liner += 1
-  elsif level1_hash.each do |k, v|
-    if k == word
-      error1 += 1
-      puts "LINE:#{liner}: '#{word}'=> replace for a more suggestive adjetive"
+    @level2_hash =
+    {
+      'playful' => ['aroused', 'cheeky'],
+      'content' => ['free', 'joyful'],
+      'interested' => ['curious', 'inquisitive'],
+      'proud' => ['succesful', 'confident'],
+      'accepted' => ['respected', 'valued'],
+      'powerful' => ['courageous', 'creative'],
+      'peaceful' => ['loving', 'thankful'],
+      'trusting' => ['sensitive', 'intimate'],
+      'optimistic' => ['hopeful', 'inspired'],
+      'lonely' => ['isolated', 'abandoned'],
+      'vulnerable' => ['victimized', 'fragile'],
+      'despair' => ['grief', 'powerless'],
+      'guilty' => ['remorseful', 'ashamed'],
+      'depressed' => ['inferior', 'empty'],
+      'hurt' => ['embarrassed', 'disappointed'],
+      'repelled' => ['horrified', 'hesitant'],
+      'awful' => ['nauseated', 'detestable'],
+      'disappointed' => ['apalled', 'revolted'],
+      'disapproving' => ['judgemental', 'embarrased'],
+      'critical' => ['sceptical', 'dismissive'],
+      'distant' => ['withdrawn', 'numb'],
+      'frustrated' => ['infuriated', 'annoyed'],
+      'aggressive' => ['provoked', 'hostile'],
+      'mad' => ['furious', 'jealous'],
+      'bitter' => ['indignat', 'violated'],
+      'humiliated' => ['disrespected', 'ridiculed'],
+      'let down' => ['betrayed', 'resentful'],
+      'threatened' => ['nervous', 'exposed'],
+      'rejected' => ['excluded', 'persecuted'],
+      'weak' => ['worthless', 'insignificant'],
+      'insecure' => ['inadequate', 'inferior'],
+      'anxious' => ['overwhelmed', 'worried'],
+      'scared' => ['helpless', 'frightened'],
+      'bored' => ['indifferent', 'apathetic'],
+      'busy' => ['pressured', 'rushed'],
+      'stressed' => ['overwhelmed', 'out of control'],
+      'tired' => ['sleepy', 'unfocussed'],
+      'startled' => ['shocked', 'dismayed'],
+      'confused' => ['disillusioned', 'perplexed'],
+      'amazed' => ['astonished', 'awe'],
+      'excited' => ['eager', 'energetic']
+    }
+  end
+  
+  def populate_array
+    until @s.eos? do
+      @s.scan(/\s+/)
+      @array << @s.scan(/\w+/)
     end
   end
-  end
-end
 
-puts "#{error1} assassinations of literature."
-
-array.each do |word|
-  if word == 'lyny'
-    liner += 1
-  elsif level2_hash.each do |k, v|
-    if k == word
-      error2 += 1
-      puts "LINE:#{liner}: '#{word}' => consider substituting for an even more descriptive word" 
+  def level1_check
+    @array.each do |word|
+      if word == 'lyny'
+        @liner += 1
+      elsif @level1_hash.each do |k, v|
+          if k == word
+            @error_level1 += 1
+            puts "LINE:#{@liner}: Lacks Articulation - replace '#{word}' for a more suggestive adjetive"
+          end
+        end
+      end
     end
   end
+
+  def level2_check
+    @array.each do |word|
+      if word == 'lyny'
+        @liner += 1
+      elsif @level2_hash.each do |k, v|
+        if k == word
+          @error_level2 += 1
+          puts "LINE:#{@liner}: Lacks Engagement - '#{word}'. The following are possible substitutes: #{v}"
+        end
+      end
+    end
   end
-end
 
-puts "#{error2} opportunities for improvement"
+  end
 
-if error1 == 0 and error2 == 0
-puts "Good job! You just earned the Hemmingway badge!\n You're on your way to literary stardom!"
+  def error_message
+    puts "#{@error_level1} assassination(s) of the written language"
+    puts "#{@error_level2} attempt(s) to bore the reader to death"
+  end
+
+  def hemmingway_badge
+    if @error_level1 == 0 and @error_level2 == 0
+      puts 'Good job! You\'ve earned the Hemmingway badge! You\'re well on your way to literary stardom!'
+    end
+  end
+
+  def lint
+    populate_array
+    level1_check
+    level2_check
+    error_message
+    hemmingway_badge
+  end
+
+
 end
