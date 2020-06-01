b7df7e4e129961a37d612af62a3812cd84e67292
Gui Recordon <guilherme.recordon@gmail.com>
2020-06-01 15:07:03 -0300

    initialize rspec inside of directory and write test cases

--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -1,9 +1,37 @@
-RSpec.describe Linter do
-  it "has a version number" do
-    expect(Linter::VERSION).not_to be nil
+require './lib/linter.rb'
+
+describe Glamourly do
+  let(:classer) { Glamourly.new('This is a bad test string') }
+
+  describe "#populate_array" do
+    it "returns an array of strings" do
+      expect(classer.populate_array).to eql(['This', 'is', 'a', 'bad', 'test', 'string'])
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#level1_check" do
+    it "increments the @liner counter by one if there is a match for lyny" do
+      classer.array = ['liny', 'line-break']
+      classer.level1_check
+      expect(classer.linter).to eql(1)
+    end
+
+    it "prints an error if a word match is found" do
+      classer.array = ['bad', 'lyny']
+      expect(classer.level1_check).to eql("LINE:1: Lacks Articulation - replace 'bad' for a more suggestive adjetive")
+    end
+  end
+
+  describe "#level2_check" do
+  it "increments the @liner counter by one if there is a match for lyny" do
+      classer.array = ['liny', 'line-break']
+      classer.level2_check
+      expect(classer.linter).to eql(1)
+  end
+
+  it "prints an error if a word match is found" do
+      classer.array = ['vulnerable', 'lyny']
+      expect(classer.level2_check).to eql("LINE:1: Lacks Engagement - 'vulnerable'. The following are possible substitutes: ['victimized', 'fragile']")
+    end
   end
 end
